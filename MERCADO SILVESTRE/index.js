18dd50040e642f5d015cddebe62523cdbdc1a942
Caio Souza <72164786+caiosouz@users.noreply.github.com>
2023-03-22 02:49:48 -0300

    Add files via upload

---
+++ MERCADO SILVESTRE/index.js
@@ -0,0 +1,6 @@
+var pergunta=confirm("Voce esta gostando da pagina?");
+if (pergunta==true) {
+	alert("Ficamos muito felizes com sua respota");
+} else {
+	alert("Poxa. Espero que goste na proxima :/");
+}
